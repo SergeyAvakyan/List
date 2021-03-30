1b861bbcd70c0ef3baeed64e5ba12093504ca928
Serhii <avakyanms@gmail.com>
2021-03-30 15:15:46 +0300

    Add DoubleLinkedList

---
+++ List.Tests/DoubleLinkedListTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List.Tests
+{
+    class DoubleLinkedListTests
+    {
+        [TestCase(3, new int[] { 1, 2 }, new int[] { 1, 2, 3 })]
+        [TestCase(5, new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(1, new int[] { }, new int[] { 1 })]
+        public void AddValue_WhenValuePassed_ShouldAddValueToLast(int value, int[] actualArray, int[] expectedArray)
+        {
+            DoubleLinkedList actual = new DoubleLinkedList(actualArray);
+
+            actual.Add(value);
+
+            Assert.AreEqual(new DoubleLinkedList(expectedArray), actual);
+        }
+    }
+}
---
+++ List/DoubleLinkedList.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace List
+{
+    public class DoubleLinkedList
+    {
+        public int Length { get; private set; }
+        public int this[int index]
+        {
+            get
+            {
+                DoubleNode current = _root;
+
+                for (int i = 1; i <= index; i++)
+                {
+                    current = current.Next;
+                }
+                return current.Value;
+            }
+            set
+            {
+                DoubleNode current = _root;
+
+                for (int i = 1; i <= index; i++)
+                {
+                    current = current.Next;
+                }
+                current.Value = value;
+            }
+        }
+
+        private DoubleNode _root;
+        private DoubleNode _tail;
+        public DoubleLinkedList()
+        {
+            Length = 0;
+            _root = null;
+            _tail = null;
+        }
+        public DoubleLinkedList(int value)
+        {
+            Length = 1;
+            _root = new DoubleNode(value);
+            _tail = _root;
+        }
+        public DoubleLinkedList(int[] values)
+        {
+            if (!(values is null))
+            {
+                Length = values.Length;
+
+                if (values.Length != 0)
+                {
+                    _root = new DoubleNode(values[0]);
+                    _tail = _root;
+
+                    for (int i = 1; i < values.Length; i++)
+                    {
+                        _tail.Next = new DoubleNode(values[i]);
+                        _tail = _tail.Next;
+                    }
+                }
+                else
+                {
+                    _root = null;
+                    _tail = null;
+                }
+            }
+        }
+        public void Add(int value)
+        {
+            if (Length != 0)
+            {
+                _tail.Next = new DoubleNode(value);
+                _tail = _tail.Next;
+            }
+            else
+            {
+                _root = new DoubleNode(value);
+                _tail = _root;
+            }
+            Length++;
+        }
+        public override string ToString()
+        {
+            if (Length != 0)
+            {
+                DoubleNode current = _root;
+                string s = current.Value + " ";
+
+                while (!(current.Next is null))
+                {
+                    current = current.Next;
+                    s += current.Value + " ";
+                }
+
+                return s;
+            }
+            else
+            {
+                return String.Empty;
+            }
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is DoubleLinkedList || obj is null)
+            {
+                DoubleLinkedList list = (DoubleLinkedList)obj;
+                bool isEqual = false;
+                if (this.Length == list.Length)
+                {
+                    isEqual = true;
+                    DoubleNode currentThis = this._root;
+                    DoubleNode currentList = list._root;
+                    while (!(currentThis is null))
+                    {
+                        if (currentThis.Value != currentList.Value)
+                        {
+                            isEqual = false;
+                            break;
+                        }
+                        currentThis = currentThis.Next;
+                        currentList = currentList.Next;
+                    }
+                }
+                return isEqual;
+            }
+
+            throw new ArgumentException("obj is not List");
+        }
+        private DoubleNode GetNodeByIndex(int index)
+        {
+            DoubleNode current = _root;
+
+            for (int i = 1; i <= index; i++)
+            {
+                current = current.Next;
+            }
+            return current;
+        }
+    }
+}
